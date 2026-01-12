3918542ad99f8c46d70ba9968015388464c85041
GuruShrihari <shrinathan006@gmail.com>
2026-01-12 14:29:57 +0530

    deployment

--- backend/main.py
+++ backend/main.py
@@ -24,8 +24,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        # Add your Vercel frontend URL here:
-        # "https://your-project.vercel.app"
+        "https://credit-card-fraud-two.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
