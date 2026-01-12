4fe7012db081ffb9420339929bb788b5fffd0838
GuruShrihari <shrinathan006@gmail.com>
2026-01-12 14:47:39 +0530

    CORS issue

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://credit-card-fraud-two.vercel.app/"
+        "https://credit-card-fraud-two.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
